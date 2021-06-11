9b326d3fde33d85f6a8cf115f56205d6b2cd3301
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-12 05:17:30 +1000

    Create Animal.java

---
+++ src/day1/chapter06/Animal.java
@@ -0,0 +1,20 @@
+package day1.chapter06;
+
+public class Animal {
+
+    public void run() {
+        System.out.println("Animal.run()");
+    }
+
+    void climb() {
+        System.out.println("Animal.climb()");
+    }
+
+    protected void sleep() {
+        System.out.println("Animal.sleep()");
+    }
+
+    private void rest() {
+        System.out.println("Animal.rest()");
+    }
+}
