802957358133b0c5cc255b3fb7c9ae48b3a4539e
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-11 18:16:47 +1000

    Update Lab04.java

--- src/day1/chapter02/Lab04.java
+++ src/day1/chapter02/Lab04.java
@@ -4,12 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lab04 {
+
+    int value;
+
     public static void main(String[] args) {
         Lab04 lab = new Lab04();
         lab.demo();
     }
 
     void demo() {
+        System.out.println("Declare and assign");
+        declareAndAssign();
+        System.out.println();
+        System.out.println("Read/write to object field");
+        readWriteToObjectField();
+        System.out.println();
         System.out.println("Java object assignment");
         javaObjectAssignment();
         System.out.println();
@@ -18,7 +27,36 @@ public class Lab04 {
 
     }
 
-    private void autoBoxingAndUnboxing() {
+    void readWriteToObjectField() {
+        Lab04 lab = new Lab04();
+
+        System.out.println("Reading from object field");
+        System.out.println("lab.value = " + lab.value);
+
+        System.out.println("Writing to object field");
+        lab.value = 300;
+
+        System.out.println("Reading from object field");
+        System.out.println("lab.value = " + lab.value);
+    }
+
+    void declareAndAssign() {
+        //Declare then assign
+        int a;
+        a = 10;
+        System.out.println("a = " + a);
+
+        //Declare and assign
+        int b = 20;
+        System.out.println("b = " + b);
+
+        //Declare and not assign - compile time error
+        int c;
+        //Variable 'c' might not have been initialized
+        //System.out.println("c = " + c);
+    }
+
+    void autoBoxingAndUnboxing() {
         Integer a = new Integer(10);
         int b = 20;
         Integer c = b; //assign Integer from int
@@ -28,7 +66,7 @@ public class Lab04 {
         System.out.println("a = " + a + ", b = " + b + ", c = " + c + ", d = " + d);
     }
 
-    private void javaObjectAssignment() {
+    void javaObjectAssignment() {
         List<Integer> list1 = new ArrayList<>();
         List<Integer> list2 = list1;
         System.out.println("list1.size() = " + list1.size() + ", list2.size() = " + list2.size());
