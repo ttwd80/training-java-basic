8480ee8e2d968e60753e143975fa9e69e0df4fe1
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-12 09:38:41 +1000

    Create Fish.java

---
+++ src/day1/chapter07/Fish.java
@@ -0,0 +1,14 @@
+package day1.chapter07;
+
+public class Fish extends Animal{
+
+    @Override
+    public void sleep() {
+        System.out.println("Fish.sleep()");
+    }
+
+    public void breathe() {
+        System.out.println("Fish.breathe()");
+    }
+
+}
