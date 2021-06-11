c905b2712507e0f70a09f53bb7c3f892db1fd7c9
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-11 22:34:06 +1000

    Create Planet.java

---
+++ src/day1/chapter06/Planet.java
@@ -0,0 +1,13 @@
+package day1.chapter06;
+
+public class Planet {
+
+    private String name;
+    private long kilometersFromTheSun;
+
+    public Planet(String name, long kilometersFromTheSun) {
+        this.name = name;
+        this.kilometersFromTheSun = kilometersFromTheSun;
+    }
+
+}
