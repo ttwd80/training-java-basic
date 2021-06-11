0fd156334524a11ad8b4bc2e22e4223bfae1c79f
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-11 22:34:01 +1000

    Create Country.java

---
+++ src/day1/chapter06/Country.java
@@ -0,0 +1,17 @@
+package day1.chapter06;
+
+public class Country {
+    private String name;
+
+    public Country() {
+        name = "Unknown";
+    }
+
+    public Country(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
