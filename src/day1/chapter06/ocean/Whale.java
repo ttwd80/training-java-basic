0620dc3b6551a0830d9d1b4c5d04fcb1ceee0147
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-12 05:17:34 +1000

    Create Whale.java

---
+++ src/day1/chapter06/ocean/Whale.java
@@ -0,0 +1,15 @@
+package day1.chapter06.ocean;
+
+public class Whale {
+    void swim() {
+        System.out.println("Whale.swim()");
+    }
+
+    protected void sleep() {
+        System.out.println("Whale.sleep()");
+    }
+
+    private void rest() {
+        System.out.println("Whale.rest()");
+    }
+}
