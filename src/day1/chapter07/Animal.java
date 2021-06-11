47cc663b273136ec5fed002807770143c410dd67
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-12 09:38:34 +1000

    Create Animal.java

---
+++ src/day1/chapter07/Animal.java
@@ -0,0 +1,17 @@
+package day1.chapter07;
+
+public class Animal {
+
+    public void eat() {
+        System.out.println("Animal.eat()");
+    }
+
+    public void sleep() {
+        System.out.println("Animal.sleep()");
+    }
+
+    public void breathe() {
+        System.out.println("Animal.breathe()");
+    }
+
+}
