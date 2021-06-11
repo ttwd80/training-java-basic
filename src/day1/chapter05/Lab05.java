ded28d63ba763649984393e837c30fcc5645bc6b
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-11 20:43:06 +1000

    Create Lab05.java

---
+++ src/day1/chapter05/Lab05.java
@@ -0,0 +1,11 @@
+package day1.chapter05;
+
+public class Lab05 {
+    public static void main(String[] args) {
+        Lab05 lab = new Lab05();
+        lab.demo();
+    }
+
+    private void demo() {
+    }
+}
