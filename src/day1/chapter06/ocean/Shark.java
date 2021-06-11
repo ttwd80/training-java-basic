4475f52399fa3a5d36f8397d2177ca6ce237dc3d
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-12 05:17:37 +1000

    Create Shark.java

---
+++ src/day1/chapter06/ocean/Shark.java
@@ -0,0 +1,17 @@
+package day1.chapter06.ocean;
+
+public class Shark {
+
+    void swim() {
+        System.out.println("Shark.swim()");
+    }
+
+    protected void sleep() {
+        System.out.println("Shark.sleep()");
+    }
+
+    private void rest() {
+        System.out.println("Shark.rest()");
+    }
+
+}
