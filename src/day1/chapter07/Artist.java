6eea1f6d33489bc36450d73e6b9af3bedbe52d54
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-12 10:30:47 +1000

    Create Artist.java

---
+++ src/day1/chapter07/Artist.java
@@ -0,0 +1,12 @@
+package day1.chapter07;
+
+public class Artist {
+
+    public String getName() {
+        return "A random artist";
+    }
+
+    public void perform() {
+        System.out.println(getName() + " is performing.");
+    }
+}
