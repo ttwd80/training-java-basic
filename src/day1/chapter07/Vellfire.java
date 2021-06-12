230233d39530b60ee54df3bf497cca0f8f543bdc
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-12 10:31:08 +1000

    Create Vellfire.java

---
+++ src/day1/chapter07/Vellfire.java
@@ -0,0 +1,13 @@
+package day1.chapter07;
+
+public class Vellfire extends Car {
+
+    public Vellfire(String name) {
+        super(name);
+    }
+
+    @Override
+    String fuelType() {
+        return "petrol";
+    }
+}
