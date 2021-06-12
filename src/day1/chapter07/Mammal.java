dea909f1c3ea9184e90bc1f20fed2a2a8dbe8c9c
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-12 10:31:04 +1000

    Create Mammal.java

---
+++ src/day1/chapter07/Mammal.java
@@ -0,0 +1,13 @@
+package day1.chapter07;
+
+public class Mammal {
+    private String name;
+
+    public Mammal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
