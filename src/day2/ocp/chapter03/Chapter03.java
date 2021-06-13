31ce9edfd9dacef060a426ca4ab15fad1c57daff
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-14 02:18:25 +1000

    Update Chapter03.java

--- src/day2/ocp/chapter03/Chapter03.java
+++ src/day2/ocp/chapter03/Chapter03.java
@@ -1,8 +1,6 @@
 package day2.ocp.chapter03;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Chapter03 {
     public static void main(String[] args) {
@@ -16,8 +14,89 @@ public class Chapter03 {
         System.out.println();
 
         System.out.println("Create and use ArrayList, TreeSet, TreeMap, and ArrayDeque objects");
-        //sambung besok
+        demoDataStructure();
 
+
+    }
+
+    private void demoDataStructure() {
+        List<String> a = new ArrayList<>();
+        a.add("1");
+        a.add("2");
+        a.add("30");
+        a.add("40");
+        a.remove(2);
+        System.out.println(a);
+
+
+        List<Integer> b = new ArrayList<>();
+        b.add(1);
+        b.add(2);
+        b.add(30);
+        b.add(40);
+        b.remove(2);
+        System.out.println(b);
+
+        List<Integer> c = new ArrayList<>();
+        c.add(Integer.valueOf(1));
+        c.add(Integer.valueOf(2));
+        c.add(Integer.valueOf(30));
+        c.add(Integer.valueOf(40));
+        c.remove(Integer.valueOf(2));
+        System.out.println(c);
+
+        Set<Integer> d = new HashSet<>();
+        d.add(1);
+        d.add(20);
+        d.add(5);
+        d.add(60);
+        d.add(5);
+        d.add(900);
+        d.add(1);
+        System.out.println(d);
+
+        Set<Integer> e = new TreeSet<>();
+        e.add(1);
+        e.add(20);
+        e.add(5);
+        e.add(60);
+        e.add(5);
+        e.add(900);
+        e.add(1);
+        System.out.println(e);
+
+        Map<String, String> map1 = new LinkedHashMap<>();
+        map1.put("my", "Malaya");
+        map1.put("sg", "Singapore");
+        map1.put("nz", "New Zealand");
+        map1.put("my", "Malaysia");
+        map1.put("au", "Australia");
+        System.out.println(map1);
+
+        Map<String, String> map2 = new TreeMap<>();
+        map2.put("my", "Malaya");
+        map2.put("sg", "Singapore");
+        map2.put("nz", "New Zealand");
+        map2.put("my", "Malaysia");
+        map2.put("au", "Australia");
+        System.out.println(map2);
+
+        Deque<Integer> q1 = new ArrayDeque<>();
+        q1.addAll(Arrays.asList(1, 20, 50, 20, 1, 200, 70, 1, 50));
+        System.out.println("Peek");
+        System.out.println(q1);
+        System.out.println(q1.peek());
+        System.out.println(q1.peek());
+        System.out.println("peekFirst");
+        System.out.println(q1);
+        System.out.println(q1.peekFirst());
+        System.out.println(q1.peekFirst());
+        System.out.println("peekLast");
+        System.out.println(q1);
+        System.out.println(q1.peekLast());
+        System.out.println(q1.peekLast());
+
+        // https://docs.oracle.com/javase/8/docs/api/java/util/Deque.html
 
     }
 
