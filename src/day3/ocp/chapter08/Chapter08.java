253aa88f79bc3c8888e14401755c4158d97c1585
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-06-14 15:10:12 +1000

    Update Chapter08.java

--- src/day3/ocp/chapter08/Chapter08.java
+++ src/day3/ocp/chapter08/Chapter08.java
@@ -1,4 +1,22 @@
 package day3.ocp.chapter08;
 
+import java.io.Console;
+
 public class Chapter08 {
+    public static void main(String[] args) {
+        Chapter08 chapter08 = new Chapter08();
+        chapter08.demo();
+    }
+
+    private void demo() {
+        System.out.println("Read and write data from the console");
+        readConsole();
+        System.out.println();
+    }
+
+    private void readConsole() {
+        Console c = System.console();
+        String s = c.readLine();
+        System.out.println("input was -> " + s);
+    }
 }
